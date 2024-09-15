17763b01ac74d24741eca36f37fc8cfd8739566c
Onur AKMESE <onur.akmese@viacesi.fr>
2024-09-15 13:22:56 +0200

    Created the simple program

--- multiplicationtable.c
+++ multiplicationtable.c
@@ -1,4 +1,33 @@
 
 /* multiplicationtable.c */
 
+#include <stdio.h>
 
+#define F fflush(stdout) 
+
+void multiplicate(int x, int y)
+{
+	int result;
+
+	result = x * y;
+
+	printf("%d x %d = %d\n",
+	x,
+	y,
+	result);
+
+	return;
+}
+
+int main()
+{
+	int table;
+	int i = 0;
+	printf("Write a multiplication table (that will multiply it to 13)\n"); F;
+	scanf("%d", &table);
+
+	for(i ; i < 13; i++)
+		multiplicate(table, i);
+	
+	return 0;
+}
